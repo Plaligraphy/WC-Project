c66564d3be4d133437fd1cb66a12a1cb39a35627
Plaligraphy <evanac21@gmail.com>
2020-10-01 13:27:33 -0700

    Automated Post!

---
+++ index.js
@@ -0,0 +1,15 @@
+function usefulLinks() {
+    
+}
+function siteRedirect() {
+
+}
+function pageChange(opt) {
+    switch(opt) {
+        case 0:
+            break;
+        case 1:
+            break;            
+    }
+
+}
